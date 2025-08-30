603205a7fc229802516755ed5387014b0c949ba5
x230-usor <moondrop6869@gmail.com>
2025-08-30 13:31:32 -0400

    Style progress

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 	const api_key = '3cb9449ad382eea08832734e94a58749';
 	const [data, setData] = useState({});	
 	const [city, setCity] = useState('');
+	const [conditionImg, setConditionImg] = useState('');
 	const url = `https://api.openweathermap.org/data/2.5/weather`; 
 	
 	// Searches for using city parameter
@@ -18,10 +19,11 @@ function App() {
 		setCity(city);
 		try {
 			// api call 	
-			const response = await axios.get(url, {
+			const dataResponse = await axios.get(url, {
 				params: { q: city, appid: api_key, units: 'imperial' }
 			});
-			setData(response.data);
+			setData(dataResponse.data);
+			setConditionImg(`https://openweathermap.org/img/wn/${dataResponse.data.weather[0].icon}@2x.png`);
 		} catch (error) { console.error(error); }
 		finally {setCity('');}
 	}
@@ -29,7 +31,7 @@ function App() {
   	return (
     	<div className="phone-frame">
 			<SearchBar onSearch={handleSearch} onChange={setCity} location={city}/>
-			<Info data={data}/>			
+			<Info data={data} backgroundImg={conditionImg}/>			
 		</div>
   	);
 }
