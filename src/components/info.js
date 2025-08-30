34fd55df7ddb507872df1f64bbff049543d51798
x230-usor <moondrop6869@gmail.com>
2025-08-30 13:57:46 -0400

    Style progress

--- src/components/info.js
+++ src/components/info.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './styles/info.css';
 
-const Info = ({data}) => {
+const Info = ({data, backgroundImg}) => {
 	const high = data.main?.temp_max != null ? `H: ${data.main.temp_max.toFixed()}` : '';
 	const low = data.main?.temp_min != null ? `L: ${data.main.temp_min.toFixed()}` : '';
-
+	
 	return (
-		<div id='weather-info' className="container">
+		<div id='info-weather' className="container"
+			style={{ backgroundImage: `url(${backgroundImg})` }}	
+		>
 			<p id="info-city">{data.name ?? ''}</p>
-			<p id="info-temp">{data.main?.temp.toFixed() ?? ''}</p>
-			<p id="info-HL">{high + ' ' + low}</p>
+			<p id="info-temp">{data.main?.temp ? `${data.main.temp.toFixed()}°F` : ''}</p>
+			<p id="info-description">{Array.isArray(data.weather) && data.weather[0]?.description ? data.weather[0].description : ''}</p>
+			<p id="info-HL">{(high ? `${high}°F` : '') + ' ' + (low ? `${low}°F` : '')}</p>
 		</div>
 	);
 }
--- src/components/search.js
+++ src/components/search.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './styles/searchbar.css'
 
 const SearchBar = ({onSearch, onChange, location}) => {
